c2c56064ab60197d95c3b18a174cc34d235b78f0
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-03-02 00:14:27 -0700

    feature: Added ability to test components
    Added the ability to test our components using Jest before pushing them to GitHub.

---
+++ src/components/Footer/Footer.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Footer from './Footer';
+
+describe('Footer', () => {
+  test('renders the Footer component', () => {
+    render(<Footer />);
+  });
+});
